8c2cc6c69da23304da52fb6d2bb34021744ad0c2
Justice-Nana.Ellis <99732730+justice-ellis@users.noreply.github.com>
2023-03-20 14:19:00 +0000

    update schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,6 +4,7 @@ const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
+    track: Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -19,6 +20,12 @@ const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The tracks complete description, can be markup format"
+    description: String
+    "The mumbr of times a track has been viewed"
+    numberOfViews: Int
+    "contains array of Module"
+    module: [Module!]!
   }
 
   "Author of a complete Track or a Module"
@@ -28,6 +35,14 @@ const typeDefs = gql`
     name: String!
     "Author's profile picture"
     photo: String
+  }
+
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
   }
 `;
 
