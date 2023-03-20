3a793bcda5a3f26b7c66c459ccbcf1003f520cb6
Justice-Nana.Ellis <99732730+justice-ellis@users.noreply.github.com>
2023-03-20 14:59:02 +0000

    track arguement

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,7 +4,7 @@ const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
-    track: Track
+    track(id: ID!): Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
